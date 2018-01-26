45d39cfd812911488ec03140715638f631e79535
Rahul Malhotra <rahulmalhotra@codeinow.com>
2018-01-26 17:43:06 +0530

    Created read branch and added state of response

--- src/aura/ContactList/ContactListHelper.js
+++ src/aura/ContactList/ContactListHelper.js
@@ -1,12 +1,18 @@
 ({
 	fetchContacts : function(component, event, helper) {
-		var action = component.get("c.getContactList");
+        var action = component.get("c.getContactList");
         var accountId = component.get("v.recordId");
         action.setParams({
             accountIds: accountId
         });
-        action.setCallback(this, function(data) {
-            var contactList = data.getReturnValue();
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if(state === 'SUCCESS') {
+                var contactList = response.getReturnValue();
+            }
+            else {
+                alert('Error in getting data');
+            }
             component.set("v.contactList",contactList);
         });
         $A.enqueueAction(action);
