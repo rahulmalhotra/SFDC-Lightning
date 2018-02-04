8460db1654a70f4eb228b2987258c309e8bd5bfc
Rahul Malhotra <rahulmalhotra@codeinow.com>
2018-02-04 19:06:43 +0530

    Added code to update contact list and toast for success and error message

--- src/aura/ContactList/ContactListController.js
+++ src/aura/ContactList/ContactListController.js
@@ -18,5 +18,29 @@
         });
         // Event fired and new contact dialog open
         createContact.fire();
+    },
+
+    // Function used to update the contacts
+    editContacts: function(component, event, helper) {
+        // Getting the button element
+        var btn = event.getSource();
+        // Getting the value in the name attribute
+        var name = btn.get('v.name');
+        // Getting the record view form and the record edit form elements
+        var recordViewForm = component.find('recordViewForm');
+        var recordEditForm = component.find('recordEditForm'); 
+        // If button is edit
+        if(name=='edit') {
+            // Hiding the recordView Form and making the recordEdit form visible
+            $A.util.addClass(recordViewForm,'formHide');
+            $A.util.removeClass(recordEditForm,'formHide');
+            // Changing the button name and label
+            btn.set('v.name','save');
+            btn.set('v.label','Save');
+        }
+        else if(name=='save') {
+            // Calling saveContacts if the button is save
+            helper.saveContacts(component, event, helper);
+        }
     }
 })
--- src/aura/ContactList/ContactListHelper.js
+++ src/aura/ContactList/ContactListHelper.js
@@ -27,5 +27,67 @@
         });
         // Adding the action variable to the global action queue
         $A.enqueueAction(action);
-	}    
+	},
+
+    // Function to update the contacts on server
+    saveContacts: function(component, event, helper) {
+        // Getting the contact list from lightning component
+        var contactList = component.get("v.contactList",contactList);
+        // Getting the recordViewForm and recordEditForm component
+        var recordViewForm = component.find('recordViewForm');
+        var recordEditForm = component.find('recordEditForm'); 
+        // Initializing the toast event to show toast
+        var toastEvent = $A.get('e.force:showToast');
+        // Defining the action to save contact List ( will call the saveContactList apex controller )
+        var saveAction = component.get("c.saveContactList");
+        // setting the params to be passed to apex controller
+        saveAction.setParams({ contactList: contactList });
+        // callback action on getting the response from server
+        saveAction.setCallback(this, function(response) {
+            // Getting the state from response
+            var state = response.getState();
+            if(state === 'SUCCESS') {
+                // Getting the response from server
+                var dataMap = response.getReturnValue();
+                // Checking if the status is success
+                if(dataMap.status=='success') {
+                    // Remove the formHide class
+                    $A.util.removeClass(recordViewForm,'formHide');
+                    // Add the formHide class
+                    $A.util.addClass(recordEditForm,'formHide');
+                    // Getting the button element
+                    var btn = event.getSource();
+                    // Setting the label and name of button back to edit
+                    btn.set('v.name','edit');
+                    btn.set('v.label','Edit');
+                    // Setting the success toast which is dismissable ( vanish on timeout or on clicking X button )
+                    toastEvent.setParams({
+                        'title': 'Success!',
+                        'type': 'success',
+                        'mode': 'dismissable',
+                        'message': 'Contacts saved successfully.'
+                    });
+                    // Fire success toast event ( Show toast )
+                    toastEvent.fire();            
+                }
+                // Checking if the status is error 
+                else if(dataMap.status=='error') {
+                    // Setting the error toast which is dismissable ( vanish on timeout or on clicking X button )
+                    toastEvent.setParams({
+                        'title': 'Success!',
+                        'type': 'error',
+                        'mode': 'dismissable',
+                        'message': 'There is an error updating the contacts.'
+                    });
+                    // Fire error toast event ( Show toast )
+                    toastEvent.fire();                
+                }
+            }
+            else {
+                // Show an alert if the state is incomplete or error
+                alert('Error in getting data');
+            }
+        });
+        $A.enqueueAction(saveAction);
+    }    
 })
